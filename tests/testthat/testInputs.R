9fd014eeed220da624e7910b742d8ca6fcd574bd
drighelli <d.righelli@gmail.com>
2017-10-20 16:33:26 -0400

    test successful

--- tests/testthat/testInputs.R
+++ tests/testthat/testInputs.R
@@ -1,13 +1,12 @@
 test_that("Loading same data bed and bam file produces the same object", {
-    bed.path <- system.file("extdata/Bed/chr19", package = "DEScan")
+    bed.path <- system.file("extdata/Bed/", package = "DEScan")
     bed.files <- list.files(bed.path, full.names = TRUE)
     bam.path <- system.file("extdata/Bam/chr19", package = "DEScan")
     bam.files <- list.files(bam.path, full.names = TRUE)
-
     bam.file <- bam.files[1]
-
     bed.file <- bed.files[1]
-
-    expect_identical(readBamAsBed(file=bam.file, chr="chr9"), readBedFile(filename=bed.file, chr="chr9"))
+    bamRange <- readBamAsBed(file=bam.file, chr="chr19")
+    bedRange <- readBedFile(filename=bed.file, chr="chr19")
+    expect_identical(bamRange@ranges, bedRange@ranges)
 })
 
