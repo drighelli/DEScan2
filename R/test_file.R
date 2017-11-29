f1702c29c0e87b8c2be120a9805892c63a34e95f
drighelli <d.righelli@gmail.com>
2017-11-29 11:58:37 -0500

    tests

--- R/test_file.R
+++ R/test_file.R
@@ -1,1 +1,71 @@
 
+testPeaksManually <- function() {
+
+
+file="/home/dario/Scrivania/Dropbox/Lavori/WCM/risso_peixoto/coding/DEScan/inst/extdata/Bed/P43615_Sample_FC1_Input_fwd_chr19_Smartfilter.bed.zip"
+
+chr="chr19"; filetype="bed"; fragmentLength=200;
+binSize=50; minWin=1; maxWin=20; genomeName="mm9";
+minCompWinWidth=5000; maxCompWinWidth=10000;
+zthresh=5; minCount=0.1; verbose=TRUE; save=FALSE
+onlyStdChrs=TRUE
+fileGRangesList <- NULL
+winVector <- c(minWin:maxWin)
+
+
+    bedGRanges <- constructBedRanges(filename=file, filetype=filetype,
+                                     genomeName=genomeName,
+                                     onlyStdChrs=onlyStdChrs)
+
+    bedGrangesChrsList <- cutGRangesPerChromosome(bedGRanges)
+    if(!is.null(chr)) bedGrangesChrsList <- keepRelevantChrs(bedGrangesChrsList, chr)
+
+    if(verbose) message("Calling Peaks on chromosomes...")
+    chrGRanges=bedGrangesChrsList[[1]]
+
+
+            runWinRleList <- computeCoverageMovingWindowOnChr(
+                chrBedGRanges=chrGRanges,
+                minWinWidth=minWin,
+                maxWinWidth=maxWin,
+                binWidth=binSize
+            )
+            minCompRunWinRleList <- computeCoverageMovingWindowOnChr(
+                chrBedGRanges=chrGRanges,
+                minWinWidth=minCompWinWidth,
+                maxWinWidth=minCompWinWidth,
+                binWidth=binSize
+            )
+            maxCompRunWinRleList <- computeCoverageMovingWindowOnChr(
+                chrBedGRanges=chrGRanges,
+                minWinWidth=maxCompWinWidth,
+                maxWinWidth=maxCompWinWidth,
+                binWidth=binSize
+            )
+            lambdaChrRleList <- computeLambdaOnChr(
+                chrGRanges=chrGRanges,
+                winVector=winVector,
+                minChrRleWComp=minCompRunWinRleList[[1]],
+                minCompWinWidth=minCompWinWidth,
+                maxChrRleWComp=maxCompRunWinRleList[[1]],
+                maxCompWinWidth=maxCompWinWidth
+            )
+
+            z <- computeZ(lambdaChrRleList=lambdaChrRleList,
+                          runWinRleList=runWinRleList,
+                          chrLength=chrGRanges@seqinfo@seqlengths,
+                          minCount=minCount, binSize=50
+            ) ######## problem on coordinates
+
+            new_s <- get_disjoint_max_win(z0=z,
+                                          sigwin=fragmentLength/binSize,
+                                          nmax=Inf, zthresh=zthresh,
+                                          two_sided=FALSE, verbose=verbose
+            )
+            chrZRanges <- createGranges(chrSeqInfo=chrGRanges@seqinfo,
+                                        starts=as.numeric(rownames(z)[new_s[,1]]),
+                                        widths=new_s[,2]*binSize,
+                                        mcolname="z-score",
+                                        mcolvalues=new_s[,3]
+            )
+}
